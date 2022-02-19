2347cd28c85d37750cf2eca22d57d3acbd4c52e3
henriquecdb <henriquecamposj@gmail.com>
2022-02-18 21:20:30 -0300

    Exibir e ordenar contatos

--- main.cpp
+++ main.cpp
@@ -2,23 +2,30 @@
 #include <vector>
 #include <fstream>
 #include <string>
+#include <iomanip>
+#include <algorithm>
 
 using namespace std;
 
 class Person
 {
+public:
     string name;
     string email;
     string phone;
     string birthday;
 
-public:
     Person(string _name, string _email, string _phone, string _birthday)
     {
         this->name = _name;
         this->email = _email;
         this->phone = _phone;
         this->birthday = _birthday;
+    }
+
+    bool operator<(Person B)
+    {
+        return this->name < B.name;
     }
 };
 
@@ -32,7 +39,7 @@ public:
         ifstream input(filename);
 
         string line;
-        while(getline(input, line))
+        while (getline(input, line))
         {
             vector<string> words;
             words.clear();
@@ -55,12 +62,31 @@ public:
                 words.push_back(tmp);
             }
 
-            for (int i = 0; i < words.size(); i++)
-            {
-                cout << words[i] << endl;
-            }
-
             contacts.push_back(Person(words[0], words[1], words[2], words[3]));
+        }
+    }
+
+    void sort_contacts()
+    {
+        sort(contacts.begin(), contacts.end());
+    }
+
+    void print_contacts()
+    {
+        cout << "----------------------------------" << endl;
+        cout << "Name"
+             << "\t\t"
+             << "Email"
+             << "\t\t\t"
+             << "Phone"
+             << "\t\t\t"
+             << "Birthday" << endl;
+        for (int i = 0; i < contacts.size(); i++)
+        {
+            cout << contacts[i].name << "\t" 
+                 << contacts[i].email << "\t" 
+                 << contacts[i].phone << "\t\t" 
+                 << contacts[i].birthday << endl;
         }
     }
 };
@@ -69,6 +95,8 @@ int main()
 {
     Agenda agenda;
     agenda.load_contacts("contacts.csv");
+    agenda.sort_contacts();
+    agenda.print_contacts();
 
     return 0;
 }
